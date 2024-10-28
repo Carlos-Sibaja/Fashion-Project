18643f24a0354ebcdd717c12fd6383384de6fab3
unknown <carlos.sibaja@gmail.com>
2024-10-27 23:42:26 -0700

    files de jose added

---
+++ src/main/java/com/example/NY5FashLink/Ny5FashLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.NY5FashLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ny5FashLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ny5FashLinkApplication.class, args);
+	}
+
+}
