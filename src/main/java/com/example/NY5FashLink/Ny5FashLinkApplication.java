cdea0b3447b24b5da46f284d47c8168375a0e214
unknown <carlos.sibaja@gmail.com>
2024-10-27 23:42:26 -0700

    just add a comment at application.java

---
+++ src/main/java/com/example/NY5FashLink/Ny5FashLinkApplication.java
@@ -0,0 +1,15 @@
+package com.example.NY5FashLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ny5FashLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ny5FashLinkApplication.class, args);
+	}
+
+	// adding code from Carlos Test on intellij.
+
+}
