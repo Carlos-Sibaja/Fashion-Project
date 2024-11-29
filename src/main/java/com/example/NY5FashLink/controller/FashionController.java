1421f91bab3e3c74862f1095ca3c4d395315d6bf
andreamll <andreamll@yahoo.com.br>
2024-11-29 10:26:21 -0800

    Adjusting index page layout.

--- src/main/java/com/example/NY5FashLink/controller/FashionController.java
+++ src/main/java/com/example/NY5FashLink/controller/FashionController.java
@@ -2,6 +2,7 @@ package com.example.NY5FashLink.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,11 @@ public class FashionController {
 
     @GetMapping
     public String fashion() {
+        return "fashion";
+    }
+
+    @PostMapping
+    public String fashionPost() {
         return "fashion";
     }
 }
